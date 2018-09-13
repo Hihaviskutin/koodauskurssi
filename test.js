5d8e11dd32836cf5be26c732d82c42a304ff3ed8
Vicky Walve <vicky.walve@gmail.com>
2018-09-13 17:37:31 +0300

    toinen kurssikerta

---
+++ test.js
@@ -0,0 +1,28 @@
+var a = 5
+var b = 5
+var c = a+b 
+var d = a-b
+var e = a*b
+var f = a/b
+var g = 3
+var h = a%g
+console.log(a, "+", b, "=", c)
+ 
+console.log(a, "-", b, "=", d)
+
+console.log(a, "*", b, "=", e)
+
+console.log(a, "/", b, "=", f)
+
+console.log("jakojäännös", a, "%", g, "=", h)
+
+console.log("    *")
+console.log("   ***")
+console.log("  *****")
+console.log(" *******")
+console.log("*********")
+
+
+
+
+
